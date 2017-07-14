522101ff15a17ff23aa4d3731af564610588af51
Amila-Rukshan <amilaruk1995@gmail.com>
2017-07-14 15:28:33 +0530

    add form

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 var socket = io();
 socket.on('connect',()=>{
   console.log('connected to server');
-  
+
 });
 socket.on('disconnect',()=>{
   console.log('disconnected from server');
@@ -9,4 +9,19 @@ socket.on('disconnect',()=>{
 
 socket.on('newMessage',function(message){
   console.log('newMessage',message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+  jQuery('#messages').append(li);
 });
+
+
+jQuery('#message-form').on('submit',function(e){
+  e.preventDefault();
+
+  socket.emit('createMessage',{
+    from:'User',
+    text:jQuery('[name=message]').val()
+  },function(){
+
+  });
+})
