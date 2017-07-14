8310f8b9b4e77bad4fe1ff6a3d0b22717a563d1f
Amila-Rukshan <amilaruk1995@gmail.com>
2017-07-14 18:57:15 +0530

    Format timestaps using moment

--- public/js/index.js
+++ public/js/index.js
@@ -8,17 +8,19 @@ socket.on('disconnect',()=>{
 });
 
 socket.on('newMessage',function(message){
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   console.log('newMessage',message);
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
   jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage',function(message){
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
   var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href',message.url);
   li.append(a);
   jQuery('#messages').append(li);
@@ -43,16 +45,16 @@ locationButton.on('click',function(){
     return alert('Geolocation not supported by your browser');
   }
 
-  //locationButton.attr('disabled',disabled).text('Sending location');
+  locationButton.attr('disabled',disabled).text('Sending location');
 
   navigator.geolocation.getCurrentPosition(function(position){
-    //locationButton.removeAttr('disabled').text('Send location');
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage',{
       latitude:position.coords.latitude,
       longitude:position.coords.longitude
     });
   },function(){
-    //locationButton.removeAttr('disabled').text('Send location');
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   });
 });
