7d5c6aa97ae874708a2c369fa538c20ef845dc54
Amila-Rukshan <amilaruk1995@gmail.com>
2017-07-14 19:51:21 +0530

    scroll thing

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket = io();
+
+function scrollToBottom(){
+  //sectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  //hieghts
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect',()=>{
   console.log('connected to server');
 
@@ -16,6 +33,7 @@ socket.on('newMessage',function(message){
     createdAt:formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 socket.on('newLocationMessage',function(message){
@@ -27,6 +45,7 @@ socket.on('newLocationMessage',function(message){
     createdAt:formattedTime
   });
   jQuery('#messages').append(html);
+  scrollToBottom();
 });
 
 jQuery('#message-form').on('submit',function(e){
