d41ff6dfd09c0e07cc17c73232d3e0e2560096fc
Amila-Rukshan <amilaruk1995@gmail.com>
2017-07-14 11:50:11 +0530

    \Emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,7 @@
 var socket = io();
 socket.on('connect',()=>{
   console.log('connected to server');
-  socket.emit('createMessage',{
-    from:'jen@gmail.com',
-    text:'yup, that works'
-  });
+  
 });
 socket.on('disconnect',()=>{
   console.log('disconnected from server');
